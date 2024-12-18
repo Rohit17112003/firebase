e87e5b42bda35c8e3190b755609acac89612d072
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2024-12-18 13:56:42 +0530

    contact slider add now

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import ContactForm from "./ContactForm";
 import Discover from "./Discover";
 import OurOffice from "./OurOffice";
+import GetTouch from "./GetTouch";
+import ContactSlider from "./ContactSlider";
+import ContactSliderTwo from "./ContactSliderTwo";
 
 const ContactUs = () => {
   return (
     <>
+      <GetTouch />
+      <ContactSlider />
       <ContactForm />
-      <Discover/>
-      <OurOffice/>
+      <ContactSliderTwo />
+      <Discover />
+      <OurOffice />
     </>
   );
 };
