5281b14147e1360c02d1f59140c1ffc14cda1fcd
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2024-12-20 02:46:42 +0530

    add agian

--- src/components/Images/OldImg.jsx
+++ src/components/Images/OldImg.jsx
@@ -1,250 +1,171 @@
-import React from 'react';
+import React from "react";
 
 const OldImg = () => {
   return (
-    <div className=' grid grid-flow-row sm:grid-cols-3 grid-cols-2 sm:gap-5 gap-2 mt-4'> 
-       <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+    <div className=" grid grid-flow-row sm:grid-cols-3 grid-cols-2 sm:gap-5 gap-2 mt-4">
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/mcbrv8c">
+          <img
+            src="https://i.ibb.co/XCjfy8C/IMG-20241219-WA0128.jpg"
+            alt="IMG-20241219-WA0128"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/sWzyCPd">
+          <img
+            src="https://i.ibb.co/mNg60BV/IMG-20241219-WA0135.jpg"
+            alt="IMG-20241219-WA0135"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/Nr9rtxS">
+          <img
+            src="https://i.ibb.co/5krkGTF/IMG-20241219-WA0142.jpg"
+            alt="IMG-20241219-WA0142"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/MfL9gB0">
+          <img
+            src="https://i.ibb.co/qCwxDJQ/IMG-20241219-WA0153.jpg"
+            alt="IMG-20241219-WA0153"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/JrrdFYx">
+          <img
+            src="https://i.ibb.co/QYYcjW8/IMG-20241219-WA0154.jpg"
+            alt="IMG-20241219-WA0154"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/xMVJWts">
+          <img
+            src="https://i.ibb.co/jL7gmP5/IMG-20241219-WA0157.jpg"
+            alt="IMG-20241219-WA0157"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/x1ygp4P">
+          <img
+            src="https://i.ibb.co/bsxvSfq/IMG-20241219-WA0160.jpg"
+            alt="IMG-20241219-WA0160"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/T8CX6zZ">
+          <img
+            src="https://i.ibb.co/7W9cP7H/IMG-20241219-WA0164.jpg"
+            alt="IMG-20241219-WA0164"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/T8hpPkX">
+          <img
+            src="https://i.ibb.co/DCKswtX/IMG-20241219-WA0163.jpg"
+            alt="IMG-20241219-WA0163"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/dQYFB50">
+          <img
+            src="https://i.ibb.co/nDJ9rLs/IMG-20241219-WA0246.jpg"
+            alt="IMG-20241219-WA0246"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/HnWLzDh">
+          <img
+            src="https://i.ibb.co/qktLsMJ/IMG-20241219-WA0257.jpg"
+            alt="IMG-20241219-WA0257"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/gdPRHWw">
+          <img
+            src="https://i.ibb.co/Jqys0p2/IMG-20241219-WA0174.jpg"
+            alt="IMG-20241219-WA0174"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/hKdycpD">
+          <img
+            className="h-full"
+            src="https://i.ibb.co/VvCtxX2/IMG-20241219-WA0182.jpg"
+            alt="IMG-20241219-WA0182"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/qnLC1qX">
+          <img
+            src="https://i.ibb.co/ZJRST4t/IMG-20241219-WA0243.jpg"
+            alt="IMG-20241219-WA0243"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/z8Vhg53">
+          <img
+            src="https://i.ibb.co/jVkvY5C/IMG-20241219-WA0245.jpg"
+            alt="IMG-20241219-WA0245"
+            border="0"
+          />
+        </a>
+      </div>
 
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-
-          <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-            <a href="https://ibb.co/7N75m9d">
-              <img
-                src="https://i.ibb.co/2YXQ4b1/IMG-20241219-WA0115.jpg"
-                alt="IMG-20241219-WA0115"
-                border="0"
-              />
-            </a>
-          </div>
-          
+      <div class="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <a href="https://ibb.co/4Wh1Cvw">
+          <img
+            className="h-full"
+            src="https://i.ibb.co/mJfNdZr/IMG-20241219-WA0193.jpg"
+            alt="IMG-20241219-WA0193"
+            border="0"
+          />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default OldImg;
