7388a755967b8173fb8f3e1765e1451e591e4f2c
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2024-12-19 13:18:03 +0530

    again

--- src/components/AboutUs/Valution.jsx
+++ src/components/AboutUs/Valution.jsx
@@ -24,7 +24,7 @@ const Valution = () => {
       <div className="w-full">
         <div className=" sm:grid grid-cols-1 sm:grid-cols-2 flex flex-col-reverse gap-5  w-full">
           <div className=" flex flex-col  gap-[1.1rem] sm:mt-0 -mt-20">
-            <h1 className="sm:text-[2.4rem] text-[2rem] font-[500] text-[#000000] mt-0 sm:mt-4">
+            <h1 className="sm:text-[2.4rem] text-[2rem] font-[500] text-[#000000] mt-0 sm:mt4">
               Our Journey
             </h1>
             <span className="text-[0.90rem] sm:text-[0.98rem] tracking-wider font-sans font-thin text-black from-neutral-50">
@@ -64,12 +64,12 @@ const Valution = () => {
               alt="herologo"
               className=" box-cover h-full rounded-[0.8rem] sm:rounded-none sm:block hidden "
             />
-            <div class="w-full h-full mt-20 rounded-lg  overflow-hidden">
-              <a href="https://ibb.co/XXyJvvJ">
+            <div class="w-full h-full mt-16 rounded-lg  overflow-hidden">
+              <a href="https://ibb.co/QmLL5BZ">
                 <img
-                  className="w-full -mt-2 rounded-lg"
-                  src="https://i.ibb.co/wyJ6xx6/IMG-20241216-WA0067.jpg"
-                  alt="IMG-20241216-WA0067"
+                className=" rounded-lg shadow shadow-gray-400"
+                  src="https://i.ibb.co/LpmmGwM/IMG-20241219-131315.jpg"
+                  alt="IMG-20241219-131315"
                   border="0"
                 />
               </a>
