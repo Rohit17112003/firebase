90e2663e6f6391bece4ae609704bd3ddd2a0dea0
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2024-12-22 21:41:23 +0530

    again

--- src/components/Videos/OldReels.jsx
+++ src/components/Videos/OldReels.jsx
@@ -2,10 +2,24 @@ import React from "react";
 
 const OldReels = () => {
   return (
-    <div>
-      <video width="21%" controls className="video_page">
-        <source src="./videos/1.mp4" type="video/mp4" />
-      </video>
+    <div className=" grid grid-flow-row grid-cols-2 sm:grid-cols-3 mt-4 sm:gap-5 gap-2">
+      <div className="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <video controls>
+          <source className="w-full" src="./videos/1.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div className="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <video controls>
+          <source className="w-full" src="./videos/1.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div className="sm:max-w-sm max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+        <video controls>
+          <source className="w-full" src="./videos/1.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 };
