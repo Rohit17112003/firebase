7b3e66d2df801f10aae40f8d2efac998d8b6d39b
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2024-12-22 21:30:17 +0530

    add videos page

--- src/components/Videos/OldReels.jsx
+++ src/components/Videos/OldReels.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const OldReels = () => {
   return (
     <div>
-       
+      <video width="21%" controls className="video_page">
+        <source src="./videos/1.mp4" type="video/mp4" />
+      </video>
     </div>
-  )
-}
+  );
+};
 
 export default OldReels;
